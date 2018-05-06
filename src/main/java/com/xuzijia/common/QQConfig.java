4adc5b9d271219efcd8d78c9ede216d840b5a0f4
xuzijia <2295443695@qq.com>
2018-05-06 23:03:03 +0800

    Update QQConfig.java

--- src/main/java/com/xuzijia/common/QQConfig.java
+++ src/main/java/com/xuzijia/common/QQConfig.java
@@ -6,6 +6,6 @@ package com.xuzijia.common;
  * 2018/4/29 14:27
  */
 public class QQConfig {
-    public final static String APP_ID="101470713";
-    public final static String APP_KEY="ac1062cd9d951ade918b4a44f5286007";
+    public final static String APP_ID="xxxxxx";
+    public final static String APP_KEY="xxxxxxxx";
 }
